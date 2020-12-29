ae65fa445473eaffb35722c14baed4606c0f3f72
platoscave <platoscave60@gmail.com>
2020-12-29 23:17:29 +0100

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  Show people that are <input v-model="age"> years old. </input>
+  <div v-for="person in people">
+    {{ person.name }}
+  </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  export default {
+    data () {
+      return {
+        //resultsPerPage: 25,
+        //currentPage: 1,
+        age: 1
+      }
+    },
+    // Use the pouch property to configure the component to (reactively) read data from pouchdb.
+    pouch: {
+      // The function returns a Mango-like selector that is run against the `people` database.
+      // The result of the query is assigned to the `people` property.
+      people: function() {
+        if (!this.age) return;
+        return {age: this.age, type: "person"}
+      },
+      // You can also specify the database dynamically (local or remote), as well as limits, skip and sort order:
+/*       peopleInOtherDatabase: function() {
+        return {
+          database: this.selectedDatabase, // you can pass a database string or a pouchdb instance
+          selector: {type: "person"},
+          sort: [{name: "asc"}],
+          limit: this.resultsPerPage,
+          skip: this.resultsPerPage * (this.currentPage - 1)
+        }
+      } */
+    }
+  })
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import PouchDB from 'pouchdb-browser'
+import pouchdbFind from 'pouchdb-find'
+import pouchdbLiveFind from 'pouchdb-live-find'
 
 Vue.config.productionTip = false
+
+PouchDB.plugin(require('pouchdb-find'));
+PouchDB.plugin(require('pouchdb-live-find'));
+
+PouchDB.plugin(pouchdbFind)
+PouchDB.plugin(pouchdbLiveFind)
+
+Vue.use(pouchVue, {
+  pouch: PouchDB, 
+  defaultDB: 'coscommonsio' //You can give it any name 
+});
 
 new Vue({
   render: h => h(App),
